13c68068201c996a6f0821d0f9bb930a038c5ab3
Nickolaj Madsen <sqlbasedk@gmail.com>
2026-09-02 20:22:40 +0200

    Add tests for GCode and header reading from BGCode fixtures

--- tests/Unit/BgCodeMetadataTest.php
+++ tests/Unit/BgCodeMetadataTest.php
@@ -19,4 +19,23 @@ final class BgCodeMetadataTest extends TestCase
         self::assertSame('PrusaSlicer 2.9.5', $metadata[FileMetadataBlock::class]['Producer']);
         self::assertSame('2026-09-01 at 18:33:30 UTC', $metadata[FileMetadataBlock::class]['Produced on']);
     }
+
+    public function testItReadsGcodeFromFixture(): void
+    {
+        $bgcode = BgCode::open(__DIR__ . '/../Fixtures/test.bgcode');
+        $gcode = iterator_to_array($bgcode->gcode(), false);
+
+        self::assertNotEmpty($gcode);
+        self::assertStringContainsString('G1', $gcode[0]);
+    }
+
+    public function testItReadsHeaderFromFixture(): void
+    {
+        $bgcode = BgCode::open(__DIR__ . '/../Fixtures/test.bgcode');
+        $header = $bgcode->header();
+
+        self::assertSame('GCDE', $header->magicString());
+        self::assertSame(1, $header->version);
+        self::assertSame(1, $header->checksumType);
+    }
 }
